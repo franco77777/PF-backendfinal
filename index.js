36928c2b3605d2abf50da81f054e8b4e158f5dc3
franco7777777 <francoaresis@gmail.com>
2022-09-19 22:49:52 -0300

    asdf

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(cors(corseoptions))
 app.use(express.json())
 app.use("/", router)
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true")
-})
+}) */
 
 const port = process.env.PORT || 3001
 
